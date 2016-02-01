2a828ca06797198f659e0af6ff84bc5d08a7c207
Cameron <m35191970@gmail.com>
2016-02-01 19:22:44 -0400

    Clarity in 77

--- ctci/Problem77.java
+++ ctci/Problem77.java
@@ -4,23 +4,31 @@ import java.util.Scanner;
 
 public class Problem77 {
 
-	public static int findODP(int n){
+	public static boolean is357(int value){
+		while(value % 3 == 0){
+			value = value / 3;
+		}
+		while(value % 5 == 0){
+			value = value / 5;
+		}
+		while(value % 7 == 0){
+			value = value / 7;
+		}
+		if(value == 1 ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	// Simplest method, proceed until we find a nth value
+	public static int find357(int n){
 		int value = -1;
 		int counter = 0;
 		int i = 0;;
 		while(counter < n){
 			i++;
-			int temp = i;
-			while(temp % 3 == 0){
-				temp = temp / 3;
-			}
-			while(temp % 5 == 0){
-				temp = temp / 5;
-			}
-			while(temp % 7 == 0){
-				temp = temp / 7;
-			}
-			if(temp == 1){
+			if(is357(i)){
 				counter++;
 				value = i;
 				System.out.println(Integer.toString(i));
@@ -37,7 +45,7 @@ public class Problem77 {
 		int n = scanner.nextInt();
 		scanner.close();
 		
-		System.out.println(Integer.toString(findODP(n)));
+		System.out.println(Integer.toString(find357(n)));
 		
 	}
 
