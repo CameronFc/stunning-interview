ffc829f321fe2b11fd6ee662775aae603ffa44e0
Cameron <cameronfrncs@gmail.com>
2017-10-10 20:04:37 -0300

    Started on doubleMedian problem

---
+++ cquestions/doubleMedian.c
@@ -0,0 +1,18 @@
+
+/*
+ * Problem: Given two sorted integer arrays of lengths M and N, find their 
+ * combined median. Can you do this in O(log(N + M)) time?
+ */
+
+#include <stdio.h>
+
+#define N = 10
+#define M = 10
+
+int main() {
+
+  printf("Nice!\n");
+
+  // Outline of algorithm :
+  // Start with two different-sized 
+}
