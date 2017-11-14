0d0c67cdb7c19f76dc702aec6634fe7f6f359f74
Cameron <cameronfrncs@gmail.com>
2017-11-14 15:42:33 -0400

    Added 1.3

---
+++ cpp/1.3.cpp
@@ -0,0 +1,52 @@
+
+#include <stdio.h>
+#include <string>
+#include <cstdint>
+#include <iostream>
+#include <queue>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::queue;
+
+class Problem {
+public:
+  static void HTMLify(string& input, int length) {
+
+    queue<int> queuedChars;
+
+    for(int i = 0; i < input.size(); i++) {
+
+      // If we read a space, save it to queue
+      if(input[i] == ' ') {
+        queuedChars.push('%');
+        queuedChars.push('2');
+        queuedChars.push('0');
+      }
+
+      // If we have nothing to replace in queue, just skip
+      if(queuedChars.empty()) {
+        continue;
+      } else {
+
+        // If we read a non-space character, save it to queue
+        if(input[i] != ' ') {
+          queuedChars.push(input[i]);
+        }
+
+        // Now replace the read character with the next char queued.
+        input[i] = queuedChars.front();
+        queuedChars.pop();
+      }
+    }
+  }
+};
+
+int main(int argc, char** argv) {
+  string input = "there are several spaces in this string.            ";
+  cout << input << endl;
+  Problem::HTMLify(input, input.size());
+  cout << input << endl;
+  return 0;
+}
