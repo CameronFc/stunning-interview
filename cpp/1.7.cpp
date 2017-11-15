847a680e973ed48558df0f732c6811d2a3d41229
Cameron <cameronfrncs@gmail.com>
2017-11-15 19:20:39 -0400

    Improved 1.7

--- cpp/1.7.cpp
+++ cpp/1.7.cpp
@@ -9,7 +9,7 @@ using std::string;
 using std::cout;
 using std::endl;
 
-#define N 3
+#define N 5
 
 class Matrix {
 public:
@@ -28,37 +28,27 @@ public:
     }
   }
 
-  void swapCells90(int row, int column) {
-    int temp = values[row][column];
-    values[row][column] = values[(size - 1) - column][row];
-    values[(size - 1) - column][row] = temp;
+  void swapCellRing(int row, int column, int oldValue, int recurse) {
+    int nextRow = (size - 1) - column;
+    int nextCol = row;
+    int stored = values[nextRow][nextCol]; // Store forward position
+    values[nextRow][nextCol] = oldValue;
+
+    if(recurse) {
+      swapCellRing(nextRow, nextCol, stored, --recurse);
+    }
   }
 
   void rotate() {
-    // Create 4 identical 'triangles' cut into the square matrix. Swap them to
-    // achieve rotation.
-    for(int side = 0; side < 3; side++) {
-      for(int row = 0; row < ((size + 1) / 2); row++) {
-        int leftBound = row;
-        // The right bound starts from the right-hand side, and is less one for
-        // each row. To avoid overlapping the triangle we are 'rotating', we
-        // subtract one more from the right bound initially.
-        int rightBound = (size - 2) - row; 
-        while(leftBound <= rightBound) {
-
-          // Swap coordinates from an orientation (side) to absolute within the
-          // matrix;
-          int adjRow = row;
-          int adjCol = leftBound;
-          for(int i = 0; i < side; i++) {
-            int temp = adjRow;
-            adjRow = (size - 1) - adjCol;
-            adjCol = temp;
-          }
-          swapCells90(adjRow,adjCol);
-
-          leftBound++;
-        }
+    for(int row = 0; row < ((size + 1) / 2); row++) {
+      int leftBound = row;
+      // The right bound starts from the right-hand side, and is less one for
+      // each row. To avoid overlapping the triangle we are 'rotating', we
+      // subtract one more from the right bound initially.
+      int rightBound = (size - 2) - row; 
+      while(leftBound <= rightBound) {
+        swapCellRing(row, leftBound, values[row][leftBound], 3);
+        leftBound++;
       }
     }
   }
