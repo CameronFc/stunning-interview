00b6f0e8278ac5362d9d88f9fe5f975d469e5250
Cameron <cameronfrncs@gmail.com>
2017-11-15 16:36:24 -0400

    fixed 1.6. Now works with longer runlengths.

--- cpp/1.6.cpp
+++ cpp/1.6.cpp
@@ -10,49 +10,45 @@ using std::endl;
 
 class Problem {
 public:
+
   static string runlengthCompress(const string input) {
 
     if(input.size() <= 2) {
       return input; // Can't possibly compress a string of this size
     }
 
-    int runs = 0; // number of 'runlengths' in the string
-    char current = input[0];
-    for(int i = 0; i < input.size(); i++) {
-      if(input[i] != current) {
-        runs++;
+    // Determine size of output string beforehand.
+    // The alternative is preallocating a string that is larger than needs be.
+    int bSize = 0; 
+    int runlength = 0;
+    for(int i = 1; i <= input.size(); i++) {
+      runlength++;
+      if(i == input.size() || input[i] != input[i - 1]) {
+        bSize += 1 + (std::to_string(runlength)).size(); 
+        runlength = 0;
       }
-      current = input[i];
     }
-    runs++; // Additional run from reading last character
 
-    int bSize = runs * 2;
     if(input.size() <= bSize) {
       return input; // Just return the input string, could not compress.
     }
-    string output(bSize, '0');
+    string output(bSize, '0'); // Create an empty string of size bSize
     
-    int oIndex = bSize - 1; // Last element.
-    current = input[input.size() - 1];
-    int runlength = 0;
-    for(int i = input.size() - 1; i >= 0 ; i--) {
-      if(current == input[i]) {
-        runlength++;
-      } else {
-        // convert runlength to char, save to spot along with character
-        char out = (char)('0' + runlength);
-        output[oIndex] = out; 
-        output[oIndex - 1] = current;
-        oIndex -= 2;
-        runlength = 1; // just saw 1 new character
+    int oIndex = 0;
+    runlength = 0;
+    for(int i = 1; i <= input.size(); i++) {
+      runlength++;
+      if(i == input.size() || input[i] != input[i - 1]) {
+        output[oIndex++] = input[i - 1]; 
+        // Convert each digit in the runlength to a character, and append it to 
+        // the output.
+        string intString = std::to_string(runlength);
+        for(int j = 0; j < intString.size(); j++) {
+          output[oIndex++] = intString[j]; 
+        }
+        runlength = 0; 
       }
-      current = input[i];
     }
-    // We have to set the last two characters written(first two characters in
-    // the string) when we hit the end of the input string.
-    output[1] = (char)('0' + runlength); 
-    output[0] = current;
-
     return output;
   }
 };
@@ -67,5 +63,7 @@ int main(int argc, char** argv) {
   result = Problem::runlengthCompress("aabcccccaaa");
   std::cout << result << std::endl;
 
+  result = Problem::runlengthCompress("aaabbbbbbbbbbbbcccccccccccccdeee");
+  std::cout << result << std::endl;
   return 0;
 }
